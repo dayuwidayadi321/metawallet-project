b7a4f02b71fb85192a96ce5c2ba575467c5017ee
dfxc indonesa security <dayu.widayadi@gmail.com>
2025-06-15 08:04:11 +0700

    Delete v.sol

--- v.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract Nuclear {
-    function nuclear(address target) public payable {
-        selfdestruct(payable(target));
-    }
-}
